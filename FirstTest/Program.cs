b018475de8bcfbe231723f305d5b6ef4166bf3d5
Noora Jokinen <k8241@student.jamk.fi>
2017-01-19 13:14:32 +0200

    Salute corrected

--- FirstTest/Program.cs
+++ FirstTest/Program.cs
@@ -10,7 +10,7 @@ namespace FirstTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello GitHub!");
+            Console.WriteLine("Hello GitHubbi!");
         }
     }
 }
